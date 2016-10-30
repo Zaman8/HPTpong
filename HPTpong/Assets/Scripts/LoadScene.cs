3649520fb0f0afe0ac22fa5899cccb513646aeff
Zaman8 <19stizai@hawken.edu>
2016-10-29 21:57:27 -0400

    Began hidden processes

---
+++ HPTpong/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+    public bool isAI;
+    public bool isHard;
+    public int max;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void loadSingle() {
+        Application.runInBackground = false;
+        Object.DontDestroyOnLoad(this.gameObject);
+        SceneManager.LoadScene("Single Player");
+    }
+
+    public void OnLevelWasLoaded(int level) {
+        if (level == 1) {
+
+        }
+    }
+}
